2cdf8c7bd07b318d706cd6f44d0245f48967bd53
Morozenko Danil <morozenko.danil@gmail.com>
2022-07-31 14:41:49 +0300

    Initial Commit

---
+++ Homework_2_Morozenko/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework_2_Morozenko
+//
+//  Created by Danil Morozenko on 31.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
